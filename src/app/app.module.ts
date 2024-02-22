a4fe123cf0ee3a11a050d64dd27184d5fb46c9e5
Agustín <amarquardt@movizen.com>
2024-02-21 23:41:12 -0300

    se importan modulos en app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CryptoListComponent } from './@components/crypto-list/crypto-list.component';
 import { CryptoDetailComponent } from './@components/crypto-detail/crypto-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { CryptoDetailComponent } from './@components/crypto-detail/crypto-detail
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TableModule,
+    TagModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
